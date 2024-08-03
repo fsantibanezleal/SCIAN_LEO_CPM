d8959e15e45f541b021cdf65cf51da864aa6ef72
Felipe Santibañez-Leal <felipe@FASL.Work>
2024-08-03 08:42:37 -0400

    Feat: Add _polyMembrane as an element for the CellWM class

--- Version Base/Functions/Physics/CellWM/cellWM.cpp
+++ Version Base/Functions/Physics/CellWM/cellWM.cpp
@@ -10,7 +10,7 @@ CellWM::CellWM()
 	_vel			= point_2D(0.0,0.0);
 
 	_nCont			= 100;
-	_paramNumFilo   = 1;
+	_paramNumFilo   = 4;
 
 	for(register int i = 0; i <_paramNumFilo; i++)
 	{
@@ -30,7 +30,7 @@ CellWM::CellWM()
 
 	_paramV0			= 1.0f;
 
-
+	_polyMembrane		= new point_2D[_nCont];
 };
 
 CellWM::~CellWM() {
@@ -45,7 +45,6 @@ void	CellWM::InitCell(void){
 	_pos			= point_2D(0.0,0.0);
 	_vel			= point_2D(0.0,0.0);
 
-	_nCont			= 100;
 	for(register int i = 0; i <_paramNumFilo; i++)
 	{
 		_angleThetha0[i]	= 0;
@@ -71,29 +70,20 @@ void	CellWM::InitCell(point_2D vPos, float vRadio){
 	_fActive		= false;
 	_bRunning		= false;
 
-	_pos			= point_2D(vPos.x(),vPos.y());
+	_pos			= point_2D(vPos.x,vPos.y);
 	_vel			= point_2D(0.0,0.0);
 
-	_nCont			= 100;
 
 	for(register int i = 0; i <_paramNumFilo; i++)
 	{
-		_angleThetha0[i]	= 0.0f;
-		_radR0[i]			= vRadio/2;
+		_radR0[i]				= vRadio;
 					
-		_paramA[i]			= 3.0f;
-		_paramW[i]			= 2.5f;
-		_paramGamma[i]		= 0.5f;
-		_paramSigma[i]		= 0.5f;
+		_angleThetha0[i]		= -(PI)* ( (float(rand())/float(RAND_MAX)) - 1.0f);
+		_paramA[i]				= vRadio * ( (float(rand())/float(RAND_MAX))) + 0.5f * vRadio;
+		_paramW[i]				= 0.8f * ( (float(rand())/float(RAND_MAX)));
+		_paramGamma[i]			= 1.5f * ( (float(rand())/float(RAND_MAX)));
+		_paramSigma[i]			= 1.8f * ( (float(rand())/float(RAND_MAX)));
 	}
-	_angleThetha0[0]		= PI/2.0f + 0.1f;
-	//_angleThetha0[1]	= PI/2.0f - 0.1f;
-
-	_paramA[0]				= vRadio/2;
-	//_paramA[1]			= vRadio/2;
-
-	_paramGamma[0]			= 1.0f;
-	_paramSigma[0]			= 1.0f;
 
 	_paramV0				= 0.01f;
 
@@ -112,8 +102,14 @@ void	CellWM::InitCell(point_2D vPos, float vRadio){
 };
 
 float	CellWM::FunP(float vValue, int index) {
-
-	return _radR0[index] + _paramA[index] * exp(-pow(vValue - _angleThetha0[index],2)/(2 * _paramW[index] * _paramW[index]));
+	float dummyAngleDist = vValue - _angleThetha0[index];
+	if( dummyAngleDist > PI){
+		dummyAngleDist = dummyAngleDist - 2.0f*PI;
+	} 
+	if( dummyAngleDist < -PI){
+		dummyAngleDist = dummyAngleDist + 2.0f*PI;
+	}
+	return _radR0[index] + _paramA[index] * exp(-pow(dummyAngleDist,2)/(2 * _paramW[index] * _paramW[index]));
 };
 
 void	CellWM::CreateContour(void) {
@@ -121,6 +117,7 @@ void	CellWM::CreateContour(void) {
 	register float radius=0.0f, tempR;
 		
 	for (register int i = 0; i < _nCont; i++) {
+		radius  = 0.0f;
 		angle	= i*(2*PI/_nCont) - PI;
 
 		for (register int j = 0; j < _paramNumFilo; j++)
@@ -131,8 +128,8 @@ void	CellWM::CreateContour(void) {
 				radius = tempR;
 			}
 		}
-		point_2D newp(_pos.x() + radius*cos(angle), _pos.y() + radius*sin(angle));
-		append(_polyMembrane.outer(), newp);
+		point_2D newp(_pos.x + radius*cos(angle), _pos.y + radius*sin(angle));
+		_polyMembrane[i].SetPoint(newp);
 	}
 };
 
@@ -142,25 +139,26 @@ void	CellWM::UdpdateAngle(void) {
 	{
 		_angleThetha0[i] = _angleThetha0[i] + _paramSigma[i] * (PI/4)* ( (float(rand())/float(RAND_MAX)) - 0.5f);
 		if( _angleThetha0[i] > PI){
-			_angleThetha0[i] = _angleThetha0[i] - PI;
+			_angleThetha0[i] = _angleThetha0[i] - 2.0f*PI;
 		} 
 		if( _angleThetha0[i] < -PI){
-			_angleThetha0[i] = _angleThetha0[i] + PI;
+			_angleThetha0[i] = _angleThetha0[i] + 2.0f*PI;
 		} 
 	}
 };
 
 void	CellWM::UpdateShape(void) {
+	register int   idxP;
 	register float angle		= 0.0f;
 	register float radius		= 0.0f;
 	register float tempRadius	= 0.0f;
 	register float tempX,tempY;
 
-	register int i = 0;
-	BOOST_FOREACH(point_2D const& pp, _polyMembrane.outer()) {
-		angle	= i*(2*PI/_nCont) - PI;
-		tempX	= pp.x() - _pos.x();
-		tempY	= pp.y() - _pos.y();
+	for(idxP = 0;idxP < _nCont; idxP++) {
+		point_2D pp(_polyMembrane[idxP]);
+		angle	= idxP*(2*PI/_nCont) - PI;
+		tempX	= pp.x - _pos.x;
+		tempY	= pp.y - _pos.y;
 
 		tempRadius = _radR0[0];
 		radius	= sqrt(tempX*tempX + tempY*tempY);
@@ -174,9 +172,7 @@ void	CellWM::UpdateShape(void) {
 			}
 		}
 		radius	= tempRadius; 
-		(_polyMembrane.outer())[i] = point_2D(_pos.x() + radius*cos(angle), _pos.y() + radius*sin(angle));
-		
-		i++;
+		_polyMembrane[idxP] = point_2D(_pos.x + radius*cos(angle), _pos.y + radius*sin(angle));
 	}
 
 };
@@ -192,72 +188,70 @@ float	CellWM::EstimateV(void){
 	//for (register int i = 0; i < _nCont; i++) {
 	//	angle	= i*(2*PI/_nCont) - PI;
 	//	radius	= FunP(angle)  - _radR0; 
-	//	point_2D newp(_vel.x() + radius*cos(angle), _vel.y() + radius*sin(angle));
+	//	point_2D newp(_vel.x + radius*cos(angle), _vel.y + radius*sin(angle));
 	//	_vel = newp;
 	//}
 
 	// Considering overlaping
 	point_2D newp(0.0f,0.0f);
 	_vel = newp;
-	register int i = 0;
+	register int idxP = 0;
 
-	BOOST_FOREACH(point_2D const& pp, _polyMembrane.outer()) {
-		angle	= i*(2*PI/_nCont) - PI;
-		tempX	= pp.x() - _pos.x();
-		tempY	= pp.y() - _pos.y();
+	for(idxP = 0;idxP < _nCont; idxP++) {
+		point_2D pp(_polyMembrane[idxP]);
+		angle	= idxP*(2*PI/_nCont) - PI;
+		tempX	= pp.x - _pos.x;
+		tempY	= pp.y - _pos.y;
 
 		radius	= sqrt(tempX*tempX + tempY*tempY);
-		point_2D newp(_vel.x() + radius*cos(angle), _vel.y() + radius*sin(angle));
+		point_2D newp(_vel.x + radius*cos(angle), _vel.y + radius*sin(angle));
 		_vel = newp;
-		i++;
 	}
-	_vel.x(_paramV0 * _vel.x());
-	_vel.y(_paramV0 * _vel.y());
+	_vel.x = _paramV0 * _vel.x;
+	_vel.y = _paramV0 * _vel.y;
 	return _paramV0 * velocity;
 };
 
 void	CellWM::UpdatePos(void) {
-	point_2D tempV(_vel.x(),_vel.y());
-	point_2D tempP(_pos.x(),_pos.y());
+	point_2D tempV(_vel.x,_vel.y);
+	point_2D tempP(_pos.x,_pos.y);
 
-	point_2D newP(tempP.x() + tempV.x(), tempP.y() + tempV.y());
+	point_2D newP(tempP.x + tempV.x, tempP.y + tempV.y);
 	_pos = newP;
 };
 
 
 void	CellWM::FixOverlap(CellWM *vValue) {
-	register int i,j;
+	register int idxPi, idxPj;
 	register float radius;
 	float t, u;
 	bool fInter = false;
 	// Considering overlaping
 
-	point_2D centerI(_pos.x(),_pos.y());
-	point_2D centerJ((vValue)->_pos.x(),(vValue)->_pos.y());
+	point_2D centerI(_pos.x,_pos.y);
+	point_2D centerJ((vValue)->_pos.x,(vValue)->_pos.y);
 
-	i = 0;
-	BOOST_FOREACH(point_2D const& pI, _polyMembrane.outer()) {
-		j = 0;
-		BOOST_FOREACH(point_2D const& pJ, (vValue)->_polyMembrane.outer()) {
+	for(idxPi = 0;idxPi < _nCont; idxPi++) {
+		point_2D pI(_polyMembrane[idxPi]);
+		for(idxPj = 0;idxPj < _nCont; idxPj++) {
+			point_2D pJ((vValue)->_polyMembrane[idxPj]);
 
 			fInter = isSegmentsIntersected(centerI, pI, centerJ, pJ ,&t ,&u);
 
 			if(fInter) {
-				point_2D newNI(pI.x() - centerI.x(), pI.y() - centerI.y());
-				radius = sqrt(newNI.x()*newNI.x() + newNI.y()*newNI.y());
+				point_2D newNI(pI.x - centerI.x, pI.y - centerI.y);
+				point_2D newNJ(pJ.x - centerJ.x, pJ.y - centerJ.y);
 
-				point_2D newNJ(pJ.x() - centerJ.x(), pJ.y() - centerJ.y());
+				radius = sqrt(newNI.x*newNI.x + newNI.y*newNI.y);
+				newNI  = point_2D(newNI.x/radius , newNI.y/radius);
+				radius = sqrt(newNJ.x*newNJ.x + newNJ.y*newNJ.y);
+				newNJ  = point_2D(newNJ.x/radius , newNJ.y/radius);
 
-				//newN = point_2D(newN.x()/radius , newN.y()/radius);
+				_polyMembrane[idxPi]		   = point_2D(centerI.x + 1*newNI.x, centerI.y + 1*newNI.y);
+				(vValue)->_polyMembrane[idxPj] = point_2D(centerJ.x + 1*newNJ.x, centerJ.y + 1*newNJ.y);
 
-				(_polyMembrane.outer())[i]           = point_2D(centerI.x() + t*newNI.x(), centerI.y() + t*newNI.y());
-				((vValue)->_polyMembrane.outer())[j] = point_2D(centerJ.x() + u*newNJ.x(), centerJ.y() + u*newNJ.y());
-
-				//(_polyMembrane.outer())[i] = point_2D(centerI.x() + t*newN.x(), centerI.y() + t*newN.y());
 			}
-			j++;
 		}
-		i++;
 	}
 
 
